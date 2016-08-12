95874d5fac1146775bbde90a2d7bc07fb40b3846
NanXiao <nan@chinadtrace.org>
2016-08-12 13:29:51 +0800

    Upload source code.

---
+++ client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"fmt"
+)
+
+type jsonReq struct {
+	url string
+	method string
+	jsonstr []byte
+}
+
+func main() {
+	request := []jsonReq{
+		{"http://localhost:8080/transactionse", "GET", nil},
+		{"http://localhost:8080/transactionservice/transaction/10", "PUT", []byte(`{ "amount": 5000, "type":"cars" }`)},
+		{"http://localhost:8080/transactionservice/transaction/11", "PUT", []byte(`{ "amount": 10000, "type": "shopping", "parent_id": 10}`)},
+		{"http://localhost:8080/transactionservice/transaction/10", "GET", nil},
+		{"http://localhost:8080/transactionservice/transaction/11", "GET", nil},
+		{"http://localhost:8080/transactionservice/types/cars", "GET", nil},
+		{"http://localhost:8080/transactionservice/sum/10", "GET", nil},
+		{"http://localhost:8080/transactionservice/sum/11", "GET", nil},
+		{"http://localhost:8080/transactionservice/sum/12", "GET", nil},
+	}
+
+	for _, r := range request {
+		fmt.Println("URL:>", r.url)
+		req, err := http.NewRequest(r.method, r.url, bytes.NewBuffer(r.jsonstr))
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(body))
+		resp.Body.Close()
+	}
+
+}
---
+++ server.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"encoding/json"
+	"strconv"
+	"sync"
+	"reflect"
+)
+
+const (
+	statusOK = "ok"
+	statusNoSpecifiedID = "Can't find the specified transaction ID"
+	statusTransactionEncodingError = "Transaction encoidng is error"
+	statusNoSpecifiedType = "Can't find the specified type"
+)
+
+type transactionID uint64
+
+type Transaction struct {
+	Amount		float64		`json:"amount"`
+	TransType	string		`json:"type"`
+	ParentID	transactionID	`json:"parent_id,omitempty"`
+}
+
+type TransactionSum struct {
+	Sum	float64	`json:"sum"`
+}
+
+type TransactionStatus struct {
+	Status	string 	`json:"status"`
+}
+
+var transactionTable map[transactionID]*Transaction
+var typeTable map[string][]transactionID
+var sumTable map[transactionID]*TransactionSum
+
+var transactionTableLock sync.RWMutex
+var typeTableLock sync.RWMutex
+var sumTableLock sync.RWMutex
+
+type routeMethod struct {
+	method      string
+	pattern     string
+	handlerFunc http.HandlerFunc
+}
+
+var routeMethods = []routeMethod {
+	{method: "PUT", pattern: "/transaction/{transaction_id}", handlerFunc: putTransactionHandler},
+	{method: "GET", pattern: "/transaction/{transaction_id}", handlerFunc: getTransactionHandler},
+	{method: "GET", pattern: "/types/{type}", handlerFunc: getTypeHandler},
+	{method: "GET", pattern: "/sum/{transaction_id}", handlerFunc: getSumHandler},
+}
+
+func convertTransactionID(s string) (transactionID, error)  {
+	v, err := strconv.ParseUint(s, 10, 64)
+	return transactionID(v), err
+}
+
+func sendResponse(w http.ResponseWriter, v interface{})  {
+	w.Header().Set("Content-Type", "application/json")
+
+	if reflect.TypeOf(v).String() == "main.TransactionStatus" {
+		if v.(TransactionStatus).Status != statusOK {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func putTransactionHandler(w http.ResponseWriter, r *http.Request) {
+	var trans Transaction
+	var duplicated bool
+
+	id, err := convertTransactionID(mux.Vars(r)["transaction_id"])
+	if err != nil {
+		sendResponse(w, TransactionStatus{statusNoSpecifiedID})
+		return
+	}
+
+	if err = json.NewDecoder(r.Body).Decode(&trans); err != nil {
+		sendResponse(w, TransactionStatus{statusTransactionEncodingError})
+		return
+	}
+
+	transactionTableLock.Lock()
+	if _, ok := transactionTable[id]; ok {
+		duplicated = true
+	}
+	transactionTable[id] = &trans
+	transactionTableLock.Unlock()
+
+	typeTableLock.Lock()
+	if !duplicated {
+		typeTable[trans.TransType] = append(typeTable[trans.TransType], id)
+	}
+	typeTableLock.Unlock()
+
+	sumTableLock.Lock()
+	if trans.ParentID != 0 {
+		if _, ok := sumTable[trans.ParentID]; ok {
+			sumTable[trans.ParentID].Sum += trans.Amount
+		} else {
+			sumTable[trans.ParentID] = &TransactionSum{Sum:trans.Amount}
+		}
+	} else {
+		sumTable[id] = &TransactionSum{Sum:trans.Amount}
+	}
+	sumTableLock.Unlock()
+
+	sendResponse(w, TransactionStatus{statusOK})
+}
+
+func getTransactionHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := convertTransactionID(mux.Vars(r)["transaction_id"])
+	if err != nil {
+		sendResponse(w, TransactionStatus{statusNoSpecifiedID})
+		return
+	}
+
+	transactionTableLock.RLock()
+	defer transactionTableLock.RUnlock()
+
+	if trans, ok := transactionTable[id]; !ok {
+		sendResponse(w, TransactionStatus{statusNoSpecifiedID})
+	} else {
+		sendResponse(w, trans)
+	}
+}
+
+func getTypeHandler(w http.ResponseWriter, r *http.Request) {
+	typeTableLock.RLock()
+	defer typeTableLock.RUnlock()
+
+	if transIDs, ok := typeTable[mux.Vars(r)["type"]]; !ok {
+		sendResponse(w, TransactionStatus{statusNoSpecifiedType})
+	} else {
+		sendResponse(w, transIDs)
+	}
+}
+
+func getSumHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := convertTransactionID(mux.Vars(r)["transaction_id"])
+	if err != nil {
+		sendResponse(w, TransactionStatus{statusNoSpecifiedID})
+		return
+	}
+
+	sumTableLock.RLock()
+	if tranSum, ok := sumTable[id]; ok {
+		sendResponse(w, tranSum)
+		sumTableLock.RUnlock()
+		return
+	}
+	sumTableLock.RUnlock()
+
+	transactionTableLock.RLock()
+	if trans, ok := transactionTable[id]; ok {
+		sendResponse(w, TransactionSum{trans.Amount})
+		transactionTableLock.RUnlock()
+		return
+	}
+	transactionTableLock.RUnlock()
+
+	sendResponse(w, TransactionStatus{statusNoSpecifiedID})
+}
+
+func main() {
+	transactionTable = make(map[transactionID]*Transaction)
+	typeTable = make(map[string][]transactionID)
+	sumTable = make(map[transactionID]*TransactionSum)
+
+	router := mux.NewRouter().PathPrefix("/transactionservice").Subrouter().StrictSlash(true)
+	for _, v := range routeMethods {
+		router.Methods(v.method).Path(v.pattern).HandlerFunc(v.handlerFunc)
+	}
+
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
